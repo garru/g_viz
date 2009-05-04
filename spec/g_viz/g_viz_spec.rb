832d5eb525eab4234b3228144c86206c44d85508
Gary Tsang <gary@seriousbusiness.com>
2009-05-04 11:24:29 -0700

    Fixing unit tests

--- spec/g_viz/g_viz_spec.rb
+++ spec/g_viz/g_viz_spec.rb
@@ -9,12 +9,12 @@ describe GViz do
                     [:sold_pens, 'Sold Pens'], [:title2, 'title2'], [:text2, 'text2']]
 
         @data = [
-          {:date => "2008-01-01", :sold_pencils => 30000, :sold_pens => 40645},
-          {:date => Date.parse("2008-01-02"), :sold_pencils => 14045, :sold_pens => 20374},
-          {:date => Date.parse("2008-01-03"), :sold_pencils => 55022, :sold_pens => 50766},
-          {:date => Date.parse("2008-01-04"), :sold_pencils => 75284, :sold_pens => 14334, :title2 => 'Out of Stock',:text2 => 'Ran out of stock on pens at 4pm'},
-          {:date => Date.parse("2008-01-05"), :sold_pencils => 41476, :sold_pens => 66467, :title1 => 'Bought Pens',:text2 => 'Bought 200k pens'},
-          {:date => Date.parse("2008-01-06"), :sold_pencils => 33322, :sold_pens => 39463, :title1 => 'Bought Pens',:text2 => 'Bought 200k pens'}
+          {:date => "2008-02-01", :sold_pencils => 30000, :sold_pens => 40645},
+          {:date => Date.parse("2008-02-02"), :sold_pencils => 14045, :sold_pens => 20374},
+          {:date => Date.parse("2008-02-03"), :sold_pencils => 55022, :sold_pens => 50766},
+          {:date => Date.parse("2008-02-04"), :sold_pencils => 75284, :sold_pens => 14334, :title2 => 'Out of Stock',:text2 => 'Ran out of stock on pens at 4pm'},
+          {:date => Date.parse("2008-02-05"), :sold_pencils => 41476, :sold_pens => 66467, :title1 => 'Bought Pens',:text2 => 'Bought 200k pens'},
+          {:date => Date.parse("2008-02-06"), :sold_pencils => 33322, :sold_pens => 39463, :title1 => 'Bought Pens',:text2 => 'Bought 200k pens'}
         ]
         GViz.add_graph('AnnotatedTimeline', @data, @mapping)
         output = GViz.output
